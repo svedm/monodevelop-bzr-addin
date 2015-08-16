9c401fb9c21ff3f221238d2e1605c2f3c2cca9fd
Incognito Svetoslav <venom008@gmail.com>
2015-08-17 02:25:57 +0500

    Skeleton project

---
+++ MonoDevelop.VersionControl.Bazaar/MonoDevelop.VersionControl.Bazaar/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin(
+	"MonoDevelop.VersionControl.Bazaar", 
+	Namespace = "MonoDevelop.VersionControl.Bazaar",
+	Version = "1.0"
+)]
+
+[assembly:AddinName("MonoDevelop.VersionControl.Bazaar")]
+[assembly:AddinCategory("IDE extensions")]
+[assembly:AddinDescription("MonoDevelop.VersionControl.Bazaar")]
+[assembly:AddinAuthor("VeNOm")]
+
