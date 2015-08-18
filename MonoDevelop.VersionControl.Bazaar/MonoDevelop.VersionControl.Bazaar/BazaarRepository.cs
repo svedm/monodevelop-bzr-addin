ef1495d15c25a69376b122b62eaef817ff1a1d64
Incognito Svetoslav <venom008@gmail.com>
2015-08-18 15:04:28 +0500

    Repository and revision improvements

--- MonoDevelop.VersionControl.Bazaar/MonoDevelop.VersionControl.Bazaar/BazaarRepository.cs
+++ MonoDevelop.VersionControl.Bazaar/MonoDevelop.VersionControl.Bazaar/BazaarRepository.cs
@@ -2,84 +2,104 @@ using System;
 using System.Collections.Generic;
 
 using MonoDevelop.Core;
+using System.IO;
 
 namespace MonoDevelop.VersionControl.Bazaar
 {
 	public class BazaarRepository : UrlBasedRepository
 	{
 		#region implemented abstract members of Repository
+
 		public override string GetBaseText(FilePath localFile)
 		{
 			throw new NotImplementedException();
 		}
+
 		protected override Revision[] OnGetHistory(FilePath localFile, Revision since)
 		{
 			throw new NotImplementedException();
 		}
+
 		protected override IEnumerable<VersionInfo> OnGetVersionInfo(IEnumerable<FilePath> paths, bool getRemoteStatus)
 		{
 			throw new NotImplementedException();
 		}
+
 		protected override VersionInfo[] OnGetDirectoryVersionInfo(FilePath localDirectory, bool getRemoteStatus, bool recursive)
 		{
 			throw new NotImplementedException();
 		}
+
 		protected override Repository OnPublish(string serverPath, FilePath localPath, FilePath[] files, string message, IProgressMonitor monitor)
 		{
 			throw new NotImplementedException();
 		}
+
 		protected override void OnUpdate(FilePath[] localPaths, bool recurse, IProgressMonitor monitor)
 		{
 			throw new NotImplementedException();
 		}
+
 		protected override void OnCommit(ChangeSet changeSet, IProgressMonitor monitor)
 		{
 			throw new NotImplementedException();
 		}
+
 		protected override void OnCheckout(FilePath targetLocalPath, Revision rev, bool recurse, IProgressMonitor monitor)
 		{
 			throw new NotImplementedException();
 		}
+
 		protected override void OnRevert(FilePath[] localPaths, bool recurse, IProgressMonitor monitor)
 		{
 			throw new NotImplementedException();
 		}
+
 		protected override void OnRevertRevision(FilePath localPath, Revision revision, IProgressMonitor monitor)
 		{
 			throw new NotImplementedException();
 		}
+
 		protected override void OnRevertToRevision(FilePath localPath, Revision revision, IProgressMonitor monitor)
 		{
 			throw new NotImplementedException();
 		}
+
 		protected override void OnAdd(FilePath[] localPaths, bool recurse, IProgressMonitor monitor)
 		{
 			throw new NotImplementedException();
 		}
+
 		protected override void OnDeleteFiles(FilePath[] localPaths, bool force, IProgressMonitor monitor, bool keepLocal)
 		{
 			throw new NotImplementedException();
 		}
+
 		protected override void OnDeleteDirectories(FilePath[] localPaths, bool force, IProgressMonitor monitor, bool keepLocal)
 		{
 			throw new NotImplementedException();
 		}
+
 		protected override string OnGetTextAtRevision(FilePath repositoryPath, Revision revision)
 		{
 			throw new NotImplementedException();
 		}
+
 		protected override RevisionPath[] OnGetRevisionChanges(Revision revision)
 		{
 			throw new NotImplementedException();
 		}
+
 		protected override void OnIgnore(FilePath[] localPath)
 		{
 			throw new NotImplementedException();
 		}
+
 		protected override void OnUnignore(FilePath[] localPath)
 		{
 			throw new NotImplementedException();
 		}
+
 		#endregion
 
 		#region implemented abstract members of UrlBasedRepository
@@ -93,6 +113,20 @@ namespace MonoDevelop.VersionControl.Bazaar
 		}
 
 		#endregion
+
+		public static string GetLocalBasePath(string localPath)
+		{
+			if (null == localPath)
+			{
+				return string.Empty;
+			}
+			if (Directory.Exists(Path.Combine(localPath, ".bzr")))
+			{
+				return localPath;
+			}
+
+			return GetLocalBasePath(Path.GetDirectoryName(localPath));
+		}
 	}
 
 }
--- MonoDevelop.VersionControl.Bazaar/MonoDevelop.VersionControl.Bazaar/BazaarRevision.cs
+++ MonoDevelop.VersionControl.Bazaar/MonoDevelop.VersionControl.Bazaar/BazaarRevision.cs
@@ -6,6 +6,8 @@ namespace MonoDevelop.VersionControl.Bazaar
 	{
 		public readonly string Rev;
 
+		public RevisionPath[] ChangedFiles	{ get;	set; }
+
 		public static readonly string HEAD = "-1";
 		public static readonly string FIRST = "1";
 		public static readonly string NONE = "NONE";
@@ -16,7 +18,7 @@ namespace MonoDevelop.VersionControl.Bazaar
 			Rev = rev;
 		}
 
-		public BazaarRevision(Repository repo, string rev, DateTime time, string author, string message)
+		public BazaarRevision(Repository repo, string rev, DateTime time, string author, string message, RevisionPath[] changedFiles)
 			: base(repo, time, author, message)
 		{
 			Rev = rev;
