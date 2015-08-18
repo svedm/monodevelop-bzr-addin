1226fda446636846b01837bafbd120ca7be8bfe7
Incognito Svetoslav <venom008@gmail.com>
2015-08-18 17:06:07 +0500

    Init handler

--- MonoDevelop.VersionControl.Bazaar/MonoDevelop.VersionControl.Bazaar/BazaarCommandHandler.cs
+++ MonoDevelop.VersionControl.Bazaar/MonoDevelop.VersionControl.Bazaar/BazaarCommandHandler.cs
@@ -151,7 +151,74 @@ namespace MonoDevelop.VersionControl.Bazaar
 				bsd.Destroy ();
 			}
 		}// OnMerge
-	}
 
+		/// <summary>
+		/// Determines whether a new repository can be created for the selected item
+		/// </summary>
+		[CommandUpdateHandler (BazaarCommands.Init)]
+		protected void CanInit (CommandInfo item)
+		{
+			if (1 == GetItems ().Count) {
+				VersionControlItem vcitem = GetItems ()[0];
+				if (vcitem.WorkspaceObject is Solution && null == vcitem.Repository) {
+					item.Visible = true;
+					return;
+				}
+			} 
+			item.Visible = false;
+		}// CanInit
+
+		/// <summary>
+		/// Initializes a new repository and adds the current solution.
+		/// </summary>
+		[CommandHandler (BazaarCommands.Init)]
+		protected void OnInit()
+		{
+			BazaarVersionControl bvc = null;
+			BazaarRepository repo = null;
+			VersionControlItem vcitem = GetItems ()[0];
+			string path = vcitem.Path;
+			List<FilePath> addFiles = null;
+			Solution solution = (Solution)vcitem.WorkspaceObject;
+
+			foreach (VersionControlSystem vcs in VersionControlService.GetVersionControlSystems ())
+				if (vcs is BazaarVersionControl)
+					bvc = (BazaarVersionControl)vcs;
+
+			if (null == bvc || !bvc.IsInstalled)
+				throw new Exception ("Can't use bazaar");
+
+			bvc.Init (path);
+
+			repo = new BazaarRepository (bvc, string.Format("file://{0}", path));
+			addFiles = GetAllFiles (solution);
+
+			repo.Add (addFiles.ToArray (), false, null);
+			solution.NeedsReload = true;
+		}// OnInit
+
+		/// <summary>
+		/// Returns a list of all files relevant to the solution
+		/// </summary>
+		private static List<FilePath> GetAllFiles (Solution s) {
+			List<FilePath> files = new List<FilePath> ();
+
+			files.Add (s.FileName);
+			foreach (Solution child in s.GetAllSolutions ()) {
+				if (s != child)
+					files.AddRange (GetAllFiles (child));
+			}// recurse!
+			foreach (Project project in s.GetAllProjects ()) {
+				files.Add (project.FileName);
+				foreach (ProjectFile pfile in project.Files) {
+					files.Add (pfile.FilePath);
+				}// add project file
+			}// add project files
+
+			return files;
+		}// GetAllFiles
+
+
+	}
 }
 
