e99b344cfce2b49958eabfe42229e8156a503453
Incognito Svetoslav <venom008@gmail.com>
2015-08-18 15:18:01 +0500

    BazaarCommands

---
+++ MonoDevelop.VersionControl.Bazaar/MonoDevelop.VersionControl.Bazaar/BazaarCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MonoDevelop.VersionControl.Bazaar
+{
+	public enum BazaarCommands
+	{
+		Resolve,
+		Pull,
+		Merge,
+		Branch,
+		Init,
+		Ignore,
+		Bind,
+		Unbind,
+		Uncommit,
+		Export,
+		Push
+	}
+}
+
