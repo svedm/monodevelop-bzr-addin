eafff2e2db0870fc6bc22b0495ebfa86a7e2c171
Incognito Svetoslav <venom008@gmail.com>
2015-08-18 15:17:46 +0500

    BazaarNodeExtension and skeleton BazaarCommandHandler

---
+++ MonoDevelop.VersionControl.Bazaar/MonoDevelop.VersionControl.Bazaar/BazaarCommandHandler.cs
@@ -0,0 +1,14 @@
+using System;
+using MonoDevelop.Ide.Gui.Components;
+using MonoDevelop.Projects;
+using MonoDevelop.Ide.Gui.Pads.ProjectPad;
+
+namespace MonoDevelop.VersionControl.Bazaar
+{
+	public class BazaarCommandHandler : VersionControlCommandHandler
+	{
+		
+	}
+
+}
+
---
+++ MonoDevelop.VersionControl.Bazaar/MonoDevelop.VersionControl.Bazaar/BazaarNodeExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MonoDevelop.Ide.Gui.Components;
+using MonoDevelop.Projects;
+using MonoDevelop.Ide.Gui.Pads.ProjectPad;
+
+namespace MonoDevelop.VersionControl.Bazaar
+{
+	public class BazaarNodeExtension : NodeBuilderExtension
+	{
+		public override bool CanBuildNode (Type dataType)
+		{
+			return typeof(ProjectFile).IsAssignableFrom (dataType)
+				|| typeof(SystemFile).IsAssignableFrom (dataType)
+				|| typeof(ProjectFolder).IsAssignableFrom (dataType)
+				|| typeof(IWorkspaceObject).IsAssignableFrom (dataType);
+		}
+
+		public override Type CommandHandlerType {
+			get { return typeof(BazaarCommandHandler); }
+		}
+	}
+}
+
